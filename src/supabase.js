0336605d8dbbfc3af4959c948a828e37c0e5e8cc
Shankar <iyerx9@gmail.com>
2026-03-10 14:41:16 +0530

    use env vars for supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://utbupoztacivqxetugdw.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0YnVwb3p0YWNpdnF4ZXR1Z2R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMjE1MzIsImV4cCI6MjA4ODY5NzUzMn0.M5N_NFQ6FXrRWR9Lb0dzVnPcKmAPBbz6hCKdsAbLhzM'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://utbupoztacivqxetugdw.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0YnVwb3p0YWNpdnF4ZXR1Z2R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMjE1MzIsImV4cCI6MjA4ODY5NzUzMn0.M5N_NFQ6FXrRWR9Lb0dzVnPcKmAPBbz6hCKdsAbLhzM'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
